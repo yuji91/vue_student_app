a50dc604aa851c7fcf9d6392631f11ccc5254836
yuji91 <yuji91.1227@gmail.com>
2019-12-28 10:59:34 +0900

    Add task new create with vuex

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,40 @@
     <h2>List of tasks</h2>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done">
+        <input type="checkbox" v-bind:checked="task.done"
+          v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
       </li>
     </ul>
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="new task">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      newTaskName: ''
+    }
+  },
   computed: {
     tasks () {
       return this.$store.state.tasks
+    }
+  },
+  methods: {
+    addTask () {
+      this.$store.commit('addTask', {
+        name: this.newTaskName
+      })
+      this.newTaskName = ''
+    },
+    toggleTaskStatus (task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -16,8 +16,28 @@ const store = new Vuex.Store({
         name: 'Purchase book about Vue.js',
         done: true
       }
-    ]
+    ],
+    nextTaskId: 3,
+  },
+  mutations: {
+    addTask (state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      state.nextTaskId++
+    },
+    toggleTaskStatus (state, { id }){
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+      filtered.forEach(task =>{
+        task.done = !task.done
+      })
+    }
   }
 })
 
 export default store
+
